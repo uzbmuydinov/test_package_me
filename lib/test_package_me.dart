99c2b678f11d85a804e12f1092d643c7f76dfd5b
uzbmuydinov@gmail.com <uzbmuydinov@gmail.com>
2022-07-15 15:00:50 +0500

    updated

--- lib/test_package_me.dart
+++ lib/test_package_me.dart
@@ -5,20 +5,24 @@ import 'package:flutter/material.dart';
 class ButtonTest extends StatelessWidget {
   final Function() onpressed;
   final Color color;
+  final String buttonText;
 
-  const ButtonTest({Key? key, required this.color, required this.onpressed}) : super(key: key);
+  const ButtonTest({Key? key, required this.color, required this.onpressed, required this.buttonText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
+
         child: MaterialButton(
-          onPressed: onpressed,color: color,
+          onPressed: onpressed,
+          color: color,
+          child: Text(buttonText),
           padding: EdgeInsets.all(8),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
-
-
           ),
         ),
       ),
